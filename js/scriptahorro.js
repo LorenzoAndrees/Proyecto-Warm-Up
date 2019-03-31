117aaad954554df75070c82e27a1f0af94369145
Lorenzo Andrees <lordandrees@gmail.com>
2019-03-31 17:52:55 -0300

    Actualizando más archivos

--- js/scriptahorro.js
+++ js/scriptahorro.js
@@ -1,18 +1,19 @@
 // start se ejecuta en el evento onload del navegador
 //window.onload = resultado();
+var button = document.getElementById("resultado");
+console.log(button);
 let loadingTx = document.getElementById('loading');
 loadingTx.style.display = 'none';
-
-var button = document.getElementById("resultado");
-button.onclick = function() {
-    var jb = document.getElementById("JB").Value;
+button.onclick = function(e) {
+    e.preventDefault();
+    let loadingTx = document.getElementById('loading');
+    var jb = document.getElementById("JB").value;
     console.log(jb);
     if (jb != undefined){
         loadingTx.style.display = 'block';
         setTimeout(() => {
             loadingTx.style.display = 'none';
         }, 3050);
-        return false;
     }
         
 }
